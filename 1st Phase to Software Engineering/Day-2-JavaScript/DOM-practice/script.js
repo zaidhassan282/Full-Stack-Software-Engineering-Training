52be17ac41e2ff80b9ca6e9eef790105c6f4194b
Zaid Hassan <zaidhassan282@gmail.com>
2026-01-21 21:55:52 +0500

    UPDATES

---
+++ 1st Phase to Software Engineering/Day-2-JavaScript/DOM-practice/script.js
@@ -0,0 +1,99 @@
+/* 1️⃣ Select & Change Text
+
+Task:
+Select a <p> element
+Change its text when a button is clicked
+
+Concepts:
+querySelector
+textContent
+addEventListener */
+
+
+// /* Select the button and paragraph elements */
+// const button = document.getElementById('change-title-btn');
+// const paragraph = document.getElementById('para-change');
+
+// /* Add a click event listener to the button */
+// button.addEventListener('click', () => {
+//   /* Change the text content of the paragraph */
+//   paragraph.textContent = 'The text has been changed!';
+// }
+// );
+
+// 2️⃣ Toggle Class
+
+// Task:
+// Click a button to toggle active class on a div
+
+// Concepts:
+// classList.add/remove/toggle
+
+
+// /* Select the button and div elements */
+// const toggleButton = document.getElementById('toggle-class-btn');
+// const boxDiv = document.getElementById('toggle-div');
+
+// /* Add a click event listener to the button */
+// toggleButton.addEventListener('click', () => {
+//     if(boxDiv.classList.contains('active')){
+//         boxDiv.classList.remove('active');
+//     }
+//     else{
+//         boxDiv.classList.add('active');
+//     }
+    
+//   /* Toggle the 'active' class on the div */
+//   boxDiv.classList.toggle('active');
+// }
+// );
+
+
+// 3️⃣ Create Elements Dynamically
+
+// Task:
+
+// Create a <li> on button click
+
+// Append it to a <ul>
+
+// Concepts:
+
+// createElement
+
+// appendChild
+
+/* Select the button and ul elements */
+
+const list = document.getElementById('list');
+const displaybtn = document.getElementById('display-input-btn');
+const inputbox = document.getElementById('user-input');
+
+displaybtn.addEventListener('click', () => {
+
+    const newItemText = inputbox.value.trim();
+
+    if(newItemText === '' ){
+        alert('Please enter in the box');
+        return;
+    }
+
+    const listItem = document.createElement('li');
+
+    const textSpan = document.createElement('span');
+    textSpan.textContent = newItemText;
+
+    list.appendChild(listItem);
+    listItem.appendChild(textSpan);
+
+    inputbox.value = '';
+    
+}
+);
+
+inputbox.addEventListener('keypress' , (event) =>{
+    if (event.key == 'Enter'){
+       displaybtn.click()
+
+    }
+})
